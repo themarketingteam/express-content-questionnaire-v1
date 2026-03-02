5c88089c2b2bed2c5dd335cf11cbde4e52db571e
Benjamin “Isaac” Hines <benjamin.hines.kaseya@gmail.com>
2026-03-02 21:12:29 +0000

    File changes

--- src/pages/Questionnaire.jsx
+++ src/pages/Questionnaire.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useMutation } from "@tanstack/react-query";
+import { base44 } from "@/api/base44Client";
 import { motion, AnimatePresence } from "framer-motion";
 import CheckboxQuestion from "../components/questionnaire/CheckboxQuestion";
 import CategorizedCheckboxQuestion from "../components/questionnaire/CategorizedCheckboxQuestion";
